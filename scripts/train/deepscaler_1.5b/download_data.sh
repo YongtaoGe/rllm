a27466713efe05ab8e6f4f459a801a5317955af8
YongtaoGe <yongtao.ge@adelaide.edu.au>
2025-08-26 08:33:58 +0000

    fix download

---
+++ scripts/train/deepscaler_1.5b/download_data.sh
@@ -0,0 +1,8 @@
+export HF_ENDPOINT=https://huggingface.co
+HF_HUB_ENABLE_HF_TRANSFER=0 \
+HF_HUB_OFFLINE=0 python scripts/data/download_hf.py \
+--dataset agentica-org/DeepScaleR-Preview-Dataset \
+--save_dir rllm/data/train/math \
+--use_hf_transfer False \
+--use_mirror False \
+--token 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
